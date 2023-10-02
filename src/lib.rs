d0abd4277d1bfadd49aecaab6bd5e92ec25af9a5
a.darafeyeu <Aliaksei_Darafeyeu@epam.com>
2023-10-02 13:31:37 +0200

    corects links

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,4 @@
 //! Azure App Configuration
-//!
-//! This crate is part of the unofficial Azure SDK effort in Rust. For more
-//! information on the project, and an overview of other crates, please refer to
-//! [our GitHub repository](https://github.com/azure/azure-sdk-for-rust).
 extern crate azure_core;
 
 pub(crate) mod auto_refresh;
